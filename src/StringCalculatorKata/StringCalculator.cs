276a32ab1ef6b0bba938fab517556036b359dc58
Carlos Schults <carlos.schults@gmail.com>
2021-04-28 17:03:49 -0300

    Green: Moved to correct implementation

--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -6,16 +6,16 @@ namespace StringCalculatorKata
     {    
         public static int Add(string numbers)
         {
-            switch (numbers.Length) {
-                case 0:
-                    return 0;
-                case 1:
-                    return int.Parse(numbers);
-                case 3:
-                    return 10;
-                default:
-                    return 14;
+            string[] parts = numbers.Split(',');
+            var result = 0;
+            
+            foreach (string part in parts)
+            {
+                if (int.TryParse(part, out int integer))
+                    result += integer;
             }
+
+            return result;
         }
     }
 }
