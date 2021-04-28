2ca4edea195190f0ead731b72f13497033d4b917
Carlos Schults <carlos.schults@gmail.com>
2021-04-28 14:22:44 -0300

    Implement method 'Add'
    The method now exists, but it just throws an exception.
    
    You can now run the test, and it fails, but it fails the "wrong" way. The
    right way would be to fail due to the assertion evaluating as false.

--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -3,6 +3,10 @@
 namespace StringCalculatorKata
 {
     public class StringCalculator
-    {        
+    {    
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }    
     }
 }
