07acadb73ff4903175e3f36aa0014ffbfbc07c48
Carlos Schults <carlos.schults@gmail.com>
2021-04-28 16:41:16 -0300

    Green: make the test pass by adding a new if

--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -6,6 +6,9 @@ namespace StringCalculatorKata
     {    
         public static int Add(string numbers)
         {
+            if (numbers.Contains('4'))
+                return 14;
+            
             if (numbers.Contains(','))
                 return 10;
 
