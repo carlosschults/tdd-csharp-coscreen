2795465f1e6fe569d20c73b05507fe08ea613bbe
Carlos Schults <carlos.schults@gmail.com>
2021-04-28 14:36:49 -0300

    Green: test passes due to a new if

--- src/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculator.cs
@@ -6,6 +6,9 @@ namespace StringCalculatorKata
     {    
         public static int Add(string numbers)
         {
+            if (numbers.Contains(','))
+                return 10;
+
             if (numbers.Length > 0)
                 return 5;
 
